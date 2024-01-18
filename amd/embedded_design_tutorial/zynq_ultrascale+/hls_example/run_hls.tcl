807d143f2f0ca1266571b6c052a7fba9b5877760
cable000 <christopher.able@gmail.com>
2024-01-18 17:15:58 -0500

    Update run_hls.tcl

--- amd/embedded_design_tutorial/zynq_ultrascale+/hls_example/run_hls.tcl
+++ amd/embedded_design_tutorial/zynq_ultrascale+/hls_example/run_hls.tcl
@@ -1,1 +1,49 @@
 
+# Create a project
+open_project -reset proj_axi_lite
+
+# Add design files
+add_files example.cpp
+# Add test bench & files
+add_files -tb example_test.cpp
+
+# Set the top-level function
+set_top example
+
+# ########################################################
+# Create a solution
+open_solution -reset solution1 -flow_target vitis
+
+# Define technology and clock rate
+set_part  {xcvu9p-flga2104-2-i}
+create_clock -period "200MHz"
+
+# Set variable to select which steps to execute
+set hls_exec 2
+
+
+csim_design
+# Set any optimization directives
+# End of directives
+
+if {$hls_exec == 1} {
+	# Run Synthesis and Exit
+	csynth_design
+	
+} elseif {$hls_exec == 2} {
+	# Run Synthesis, RTL Simulation and Exit
+	csynth_design
+	
+	cosim_design
+} elseif {$hls_exec == 3} { 
+	# Run Synthesis, RTL Simulation, RTL implementation and Exit
+	csynth_design
+	
+	cosim_design
+	export_design -format ip_catalog
+} else {
+	# Default is to exit after setup
+	csynth_design
+}
+
+exit
